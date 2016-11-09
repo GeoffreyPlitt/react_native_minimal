660cb54052e6bcffe4cef62547c0ea0cbd098140
Geoffrey Plitt <geoff@gweb.org>
2016-11-09 15:02:37 -0800

    trying to reproduce a bug, not working

--- index.android.js
+++ index.android.js
@@ -9,25 +9,32 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 class react_native_minimal extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TouchableHighlight onPress={this._onPressButton}>
+          <Text>Click Me to Fetch</Text>
+        </TouchableHighlight>
       </View>
     );
+  }
+
+  _onPressButton() {
+    const url = 'https://res.cloudinary.com/vista-stardust/video/upload/c_fill,e_fade:500,h_1280,w_720/v1477443555/vaijhhcbwwikagus4uct.m3u8'
+    fetch(url)
+    .then(console.log)
+    .catch(console.error)
+    fetch(url)
+    .then(console.log)
+    .catch(console.error)
+    fetch(url)
+    .then(console.log)
+    .catch(console.error)
   }
 }
 
